3f7c6cbd026cead3302d2c36c9028bb53fe6cd2e
goldmund <matthewtzito@gmail.com>
2021-10-02 22:05:20 -0700

    chore: add a11y and hooks rules to react config

--- packages/react/index.js
+++ packages/react/index.js
@@ -1,17 +1,19 @@
 module.exports = {
 	extends: [
-		'plugin:react/recommended',
-		'@magister_zito/eslint-config-typescript'
+		"plugin:react/recommended",
+		"plugin:jsx-a11y/recommended",
+		"@magister_zito/eslint-config-typescript",
 	],
 	settings: {
 		react: {
-			version: '17.0'
-		}
+			version: "17.0",
+		},
 	},
+	plugins: ["jsx-a11y", "react/hooks"],
 	rules: {
-		'jsx-quotes': [
-			'error',
-			'prefer-double'
-		]
-	}
+		"jsx-quotes": ["error", "prefer-double"],
+		"react/react-in-jsx-scope": "off",
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
+	},
 };

55d27a81cd09ad04d95b821d7e5c4cc174c75bd0
Matthew Zito <exbotanical@protonmail.com>
2025-03-14 23:23:23 -0700

    chore: increase test timeout for gh runner

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    testTimeout: 60_000,
     forceRerunTriggers: [
       '**/package.json/**',
       '**/vitest.config.*/**',
